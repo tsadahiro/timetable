f87b2c32aa0cf09878a6a020b02661d8dfb2274e
tsadahiro <sadahiro@tsuda.ac.jp>
2025-10-26 19:34:39 +0900

    built

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,14 +2,26 @@ import { supabase } from "../lib/supabaseClient";
 import { Button, Box, Typography } from "@mui/material";
 
 export default function LoginPage() {
-  const handleLogin = async () => {
-    await supabase.auth.signInWithOAuth({
-      provider: "google",
-      options: {
-        redirectTo: window.location.origin + "/timetable",
-      },
-    });
-  };
+const handleLogin = async () => {
+  await supabase.auth.signInWithOAuth({
+    provider: "google",
+    options: {
+      redirectTo:
+        import.meta.env.MODE === "development"
+          ? "http://localhost:5173/timetable"
+          : "https://timetable-neon.vercel.app/timetable",
+    },
+  });
+};
+  
+  //const handleLogin = async () => {
+  //  await supabase.auth.signInWithOAuth({
+  //    provider: "google",
+  //    options: {
+  //      redirectTo: window.location.origin + "/timetable",
+  //    },
+  //  });
+  //};
 
   return (
     <Box sx={{ textAlign: "center", mt: "30vh" }}>
